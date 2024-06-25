e1e704bcfe7e45dad2d2d247eb9ed3a9f5d205d3
gabriielf54 <gabriel.nuno22@gmail.com>
2024-06-24 21:52:09 -0300

    atualizar rotas e navbar para home

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Route, HashRouter, Routes } from 'react-router-dom';
-import Login from '../views/login';
-import CadastroUsuario from '../views/cadastro-usuario';
+import { Route, HashRouter, Routes } from "react-router-dom";
+import Login from "../views/login";
+import Home from "../views/home";
+import CadastroUsuario from "../views/cadastro-usuario";
 
 function Rotas() {
-    return (
-        <HashRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
-            </Routes>
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default Rotas;
